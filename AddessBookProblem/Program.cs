babfcfaaa25c588e3a70f7db014788b057ea5594
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-13 21:44:52 +0530

    Added Creating comtact of a person program

--- AddessBookProblem/Program.cs
+++ AddessBookProblem/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using AddessBookProblem;
+
+namespace AddressBookProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Displaying Welcome to Address Book Program");
+
+           
+
+        }
+    }
+}
---
+++ AddessBookProblem/UC_1CreateContact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddessBookProblem
+{
+    internal class UC_1CreateContact
+    {
+     
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public int Phonenumber { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zipcode { get; set; }
+        public string Address { get; set; }
+
+      
+    }
+}
