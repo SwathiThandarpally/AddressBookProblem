c44006f2f0bc00c7b44415704e3d90e1f99cbb45
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-13 22:36:24 +0530

    Added edit program

--- AddessBookProblem/Program.cs
+++ AddessBookProblem/Program.cs
@@ -9,15 +9,33 @@ namespace AddressBookProblem
         {
             Console.WriteLine("Displaying Welcome to Address Book Program");
 
-            //UC_2UC_2AddContact.ContactDetails();
-            
+            //UC_2UC_2
+            //AddContact.ContactDetails();
+            UC_1CreateContact UC_1CreateContact = new UC_1CreateContact();
+            bool flag = true;
+            while (flag)
+            {
+                //Console.WriteLine("1. Create  2. Edit  3.Delete ");
 
-            //UC_3
-            Edit edit = new Edit();
-            edit.createContact();
-            edit.print();
-            edit.Editing("Swathi");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        UC_1CreateContact.UC_2AddContact();
+                        break;
+                    case 2:
+                        Console.Write("Enter FirstName and LastName to edit ");
+                        string edit = Console.ReadLine();
+                        UC_1CreateContact.Equals(edit);
+                        break;
+                    default:
+                        flag = false;
+                        break;
 
+
+                }
+
+            }
         }
     }
 }
